711feb377ca2d926f852f5e9e8678f8555b72f2e
Andrew Champ <AndrewChamp@users.noreply.github.com>
2020-11-10 10:24:25 -0500

    Create class.autoload.php

---
+++ class.autoload.php
@@ -0,0 +1,41 @@
+<?php
+
+	/**
+	 *	NOTE: 		The $_directories paramater needs to be an absolute server path to the file.
+	 *	Example:	'/home/user/public_html/file.php'
+	 */
+
+	class autoload{
+		
+		public $directories;
+		
+		public function __construct($_directories){
+			$this->directories = (array)$_directories;
+			$this->autoload_register();
+		}
+		
+		
+		private function autoload_register(){
+			spl_autoload_register(function($class){
+				foreach($this->directories as $dir):
+					if(file_exists($dir.'class.'.$class.'.php') && $this->preset($dir.'class.'.$class.'.php')):
+						require($dir.'class.'.$class.'.php');
+						return;
+					endif;
+					return false;
+				endforeach;
+			});
+		}
+		
+		
+		private function preset($file){
+			if(!in_array($file, get_included_files()))
+				return true;
+				
+			return false;
+		}
+		
+
+	}
+
+?>
